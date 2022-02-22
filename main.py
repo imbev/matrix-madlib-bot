1cb0762091e29a46f2d40c9e5790ce564f8a7fef
krazykirby99999 <krazykirby99999@gmail.com>
2022-02-22 09:53:02 -0600

    Misc improvements to main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 import simplematrixbotlib as botlib
+from dotenv import load_dotenv
 import madlib_bot
 
 
-if __name__ == '__main__':
+def main():
+    load_dotenv()
     HOMESERVER = os.environ.get('HOMESERVER')
-    USERNAME = os.environ.get('USERNAME')
+    USERNAME = os.environ.get('BOT_USERNAME')
+    PASSWORD = os.environ.get('PASSWORD')
     TOKEN = os.environ.get('TOKEN')
+    PREFIX = os.environ.get('PREFIX')
+
     creds = botlib.Creds(
-        HOMESERVER, USERNAME, access_token=TOKEN)
+        HOMESERVER, USERNAME, password=PASSWORD, access_token=TOKEN)
     bot = botlib.Bot(creds)
 
     madlib_bot.setup_bot(bot)
     bot.run()
+
+
+if __name__ == '__main__':
+    main()
